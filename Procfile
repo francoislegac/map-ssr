8886ddb12f9f41a42714becb843329e20f09235d
François LE GAC - Ext <francois.le-gac@bioserenity.com>
2021-12-07 16:28:36 +0100

    Cleaned the folder

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bokeh serve --port=$PORT --allow-websocket-origin=desolate-tundra-15287.herokuapp.com --address=0.0.0.0 --use-xheaders appbis.py
+web: bokeh serve --port=$PORT --allow-websocket-origin=desolate-tundra-15287.herokuapp.com --address=0.0.0.0 --use-xheaders app.py
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bokeh.plotting import figure, show
 from bokeh.models import (GeoJSONDataSource, HoverTool, Div, 
                           Select, RadioGroup, Paragraph, 
                           MultiChoice, CustomJS, ColumnDataSource, DataTable,
-                         TableColumn)
+                         TableColumn, Dropdown, NumeralTickFormatter)
 from pprint import pprint
 from shapely.geometry import Point
 import pandas as pd
@@ -16,14 +16,17 @@ departement_source = GeoJSONDataSource(geojson=departement.to_json())
 CRS = departement.crs
 coordght = pd.read_csv('data/coordght.csv', index_col=0)
 geometry = [Point(xy) for xy in zip(coordght.longitude, coordght.latitude)]
-etablissement = gpd.GeoDataFrame(coordght.drop(['longitude', 'latitude'], axis=1), crs=CRS, geometry=geometry)
-point_source = GeoJSONDataSource(geojson=etablissement.to_json())
-test = GeoJSONDataSource(geojson=
-    gpd.GeoDataFrame([], columns=columns, geometry=[])
-)
+etablissement = gpd.GeoDataFrame(coordght, crs=CRS, geometry=geometry)
+etablissement['x'] = etablissement['geometry'].x
+etablissement['y'] = etablissement['geometry'].y
+etablissement = etablissement.drop(['geometry'], axis=1)
+flux = pd.read_csv('data/flux.csv', index_col=0)
+
+point_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
+satellite_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
 
 p = figure(
-    title='New test map',
+    title='Bourgogne-Franche-Comté',
     x_axis_location=None, 
     y_axis_location=None,
     #sizing_mode='stretch_width',
@@ -41,20 +44,30 @@ patches = p.patches('xs',
           line_width=0.5
 )
 
-circles = p.circle(
+c1 = p.circle(
     'x',
     'y',
     source=point_source,
     color='red', 
-    size=6,
-    hover_color='yellow'
+    size=10,
+    hover_color='green'
 )
 
-text = p.text(
+c2 = p.circle(
+    'x',
+    'y',
+    source=satellite_source, 
+    color='yellow',
+    size=6,
+    hover_color='green'
+)
+
+
+t1 = p.text(
     'x',
     'y',
     #color='red',
-    text_font_size={'value':'5px'},
+    text_font_size={'value':'8px'},
     text='etablissement',
     source=point_source,
 )
@@ -64,11 +77,11 @@ TOOLTIPS = [
     ('etablissement', '@etablissement'),
     ('lieu', '@lieu'),
     ('ght', '@nom_ght'),
-    ('coordonees', '($x, $y)'),
+    ('coordonées', '($x, $y)'),
 ]
 ht = HoverTool(
     tooltips= TOOLTIPS,
-    renderers = [circles]
+    renderers = [c1, c2]
 )
 p.tools.append(ht)
 
@@ -76,69 +89,128 @@ p.tools.append(ht)
 ght_names = coordght.nom_ght.drop_duplicates().sort_values().tolist()
 ght_names.append('--')
 ght_names.append('ALL')
-select = Select(
-    title="GHT:", 
+select_ght = Select(
+    title="GHT :", 
     value=ght_names[-1], 
     options=ght_names,
-    width=375,
+    width=150,
 )
 
 #define multi-choice etablissement widget
 etab_names = coordght.etablissement.drop_duplicates().tolist()
 mc = MultiChoice(
+    title='Etablissement :',
     value=[''], 
     options=etab_names,
-    width=375,
+    width=300,
+)
+
+#define specialite filter
+spe_names = flux.specialite.drop_duplicates().sort_values().fillna('Inconnu').tolist()
+spe_names.append('--')
+select_spe = Select(
+    title="Specialité :", 
+    value=spe_names[-1], 
+    options=spe_names,
+    width=150,
 )
 
 #define table widget
-flux = pd.read_csv('data/flux.csv', index_col=0)
-table_source = ColumnDataSource(flux)
+table_source = ColumnDataSource(data=dict(provance=[], effectif_transfert=[], perc_transfert=[]))
 columns = [
-    TableColumn(field='etablissement', title='etablissement'),
-    TableColumn(field='specialite', title='specialite'),
-    TableColumn(field='provenance', title='provenance'),
-    TableColumn(field='effectif_transfert', title='effectif_transfert'), #formatter=
+    TableColumn(field='provenance', title='Provenance'),
+    TableColumn(field='effectif_transfert', title='Effectif transfert'),
+    TableColumn(field='perc_transfert', title='Pourc. transfert') #, formatter=NumeralTickFormatter(format='0 %')),
 ]
 table = DataTable(
     source=table_source, 
     columns=columns,
-    width=375,
+    width=500,
 )
+
+#define paragraph
+paragraph = Paragraph(text="""Your text is initialized with the 'text' argument.  The
+remaining Paragraph arguments are 'width' and 'height'. For this example, those values
+are 200 and 100, respectively.""",
+width=200, height=100)
+
+#define vbar
+vbar_source = ColumnDataSource(data=dict(specialite=[], effectif_transfert=[]))
+p2 = figure(
+    x_range=[], 
+    title='Spécialité', 
+    width=500,
+    toolbar_location=None,
+    height=300,
+    y_axis_label='Effectif transfert',
+)
+p2.vbar(
+    x='specialite',
+    top='effectif_transfert',
+    width=0.4,
+    source = vbar_source,
+    fill_color= 'salmon',
+    line_alpha=.5,
+    #width=375,
+)
+p2.xaxis.major_label_orientation = 1.2
+
 
 #define callbaks
 def update_ght(attr, old, new):
     #reinitialize etablissement filter
     mc.value = ['']
+    select_spe.value = '--'
+    #reinitialize both data source
+    #point_source.data = 
+    #satellite_source.data = 
     #update_etablissement()
-    if select.value=='ALL':
-        filtre = etablissement
-    elif select.value=='--':
-        #filtre = gpd.GeoDataFrame([np.nan], columns=etablissement.columns, geometry=etablissement.geometry[0])
-        point_source = 
+    if select_ght.value=='ALL':
+        filtre = etablissement.copy()
     else:
-        filtre = etablissement.loc[etablissement.nom_ght==select.value, :]
-    point_source.geojson = filtre.to_json()
+        filtre = etablissement.loc[etablissement.nom_ght==select_ght.value, :]
+    point_source.data = filtre
 
 def update_etablissement(attr, old, new):
-    if mc.value == ['']:
-        pass 
-    #if type(mc.value) != list: list(mc.value) #not sure is necessary
-    else:
-        point_source.geojson = etablissement\
-        .loc[etablissement.etablissement.isin(mc.value), :].to_json()
-        table_source.data = flux.loc[flux.etablissement.isin(mc.value), :]
     #reinitialize ght filter
-    #select.value = '--'
+    select_ght.value = '--'
+    select_spe.value = '--'
+    #update map
+    point_source.data = etablissement\
+    .loc[etablissement.etablissement.isin(mc.value), :]
+    #update table
+    tmp = flux.loc[flux.etablissement.isin(mc.value), :]
+    agg_df = tmp.groupby(['provenance'])['effectif_transfert'].sum().sort_values(ascending=False).reset_index()
+    agg_df['perc_transfert'] = round((agg_df['effectif_transfert'] / agg_df['effectif_transfert'].sum())*100,1)
+    
+    table_source.data = agg_df.copy()
+
+    #add the satellites on the graph
+    satellite_names = tmp['provenance'].drop_duplicates()
+    satellite_source.data = etablissement.loc[etablissement.etablissement.isin(satellite_names),:]
+
+    #update vbar
+    agg_df = tmp.groupby(['specialite'])['effectif_transfert']\
+    .sum().reset_index().sort_values(by='effectif_transfert', ascending=False)
+    vbar_source.data = agg_df.copy()
+    p2.x_range.factors = agg_df['specialite'].tolist()
 
 
+def update_spe(attr, old, new):
+    #reinitialize other filters
+    mc.value = ['']
+    select_ght.value = '--'
+    #filter the map by specialite
+    names = flux.loc[flux.specialite==select_spe.value,'etablissement'].drop_duplicates()
+    filtre = pd.merge(etablissement, names, how='inner')
+    point_source.data = filtre
 
-select.on_change('value', update_ght)
+select_ght.on_change('value', update_ght)
+select_spe.on_change('value', update_spe)
 mc.on_change('value', update_etablissement)
 
 #define layout
-widgets = column(select, mc, table)
+widgets = column(row(select_ght, mc), row(select_spe, paragraph), p2, table)
 layout = row(p, widgets)
-
-#show(layout)
 curdoc().add_root(layout)
+curdoc().title = 'Cartographie SSR'
