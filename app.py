035323e11dd5b881827a73a8faa12ef4f4ec4051
François LE GAC - Ext <francois.le-gac@bioserenity.com>
2021-12-20 16:34:04 +0100

    version 2 the carto

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bokeh.plotting import figure, show
 from bokeh.models import (GeoJSONDataSource, HoverTool, Div, 
                           Select, RadioGroup, Paragraph, 
                           MultiChoice, CustomJS, ColumnDataSource, DataTable,
-                         TableColumn, Dropdown, NumeralTickFormatter)
+                         TableColumn, Dropdown, NumeralTickFormatter, RadioGroup)
 from pprint import pprint
 from shapely.geometry import Point
 import pandas as pd
@@ -22,8 +22,9 @@ etablissement['y'] = etablissement['geometry'].y
 etablissement = etablissement.drop(['geometry'], axis=1)
 flux = pd.read_csv('data/flux.csv', index_col=0)
 
-point_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
-satellite_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
+etab_interet_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
+etab_provenance_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
+etab_sortie_source = ColumnDataSource(data=pd.DataFrame([], columns=etablissement.columns))
 
 p = figure(
     title='Bourgogne-Franche-Comté',
@@ -47,21 +48,32 @@ patches = p.patches('xs',
 c1 = p.circle(
     'x',
     'y',
-    source=point_source,
+    source=etab_interet_source,
     color='red', 
     size=10,
-    hover_color='green'
+    hover_color='green',
+    legend_label='Etablissement d\'intérêt',
 )
 
 c2 = p.circle(
     'x',
     'y',
-    source=satellite_source, 
+    source=etab_provenance_source, 
     color='yellow',
     size=6,
-    hover_color='green'
+    hover_color='green',
+    legend_label='Etablissement de provenance',
 )
 
+c3 = p.circle(
+    'x',
+    'y',
+    source = etab_sortie_source,
+    color='orange',
+    size=6,
+    hover_color='green',
+    legend_label = 'Etablissement de sortie'
+)
 
 t1 = p.text(
     'x',
@@ -69,7 +81,7 @@ t1 = p.text(
     #color='red',
     text_font_size={'value':'8px'},
     text='etablissement',
-    source=point_source,
+    source=etab_interet_source,
 )
 
 #define hoover
@@ -77,11 +89,11 @@ TOOLTIPS = [
     ('etablissement', '@etablissement'),
     ('lieu', '@lieu'),
     ('ght', '@nom_ght'),
-    ('coordonées', '($x, $y)'),
+    #('coordonées', '($x, $y)'),
 ]
 ht = HoverTool(
     tooltips= TOOLTIPS,
-    renderers = [c1, c2]
+    renderers = [c1, c2, c3]
 )
 p.tools.append(ht)
 
@@ -118,7 +130,7 @@ select_spe = Select(
 #define table widget
 table_source = ColumnDataSource(data=dict(provance=[], effectif_transfert=[], perc_transfert=[]))
 columns = [
-    TableColumn(field='provenance', title='Provenance'),
+    TableColumn(field='provenance', title='Provenance / sortie'),
     TableColumn(field='effectif_transfert', title='Effectif transfert'),
     TableColumn(field='perc_transfert', title='Pourc. transfert') #, formatter=NumeralTickFormatter(format='0 %')),
 ]
@@ -135,12 +147,18 @@ Source: https://www.scansante.fr/applications/flux-entre-etablissements-orfee
 width=300, height=100)
 
 #define vbar
-vbar_source = ColumnDataSource(data=dict(specialite=[], effectif_transfert=[]))
+vbar_source = ColumnDataSource(data=dict(specialite=[], effectif_transfert=[], provenance=[]))
+TOOLTIPS = [
+    #('etablissement', '@etablissement'),
+    ('details', '@details')
+]
+
 p2 = figure(
     x_range=[], 
     title='Spécialité', 
     width=500,
     toolbar_location=None,
+    tooltips = TOOLTIPS,
     height=300,
     y_axis_label='Effectif transfert',
 )
@@ -155,45 +173,74 @@ p2.vbar(
 )
 p2.xaxis.major_label_orientation = 1.2
 
+#define radiogroup
+LABELS = ['Transferts entrants', 'Transferts sortants']
+
+radio_group = RadioGroup(labels=LABELS, active=0)
+
 
 #define callbaks
 def update_ght(attr, old, new):
     #reinitialize etablissement filter
     mc.value = ['']
     select_spe.value = '--'
-    #reinitialize both data source
-    #point_source.data = 
-    #satellite_source.data = 
-    #update_etablissement()
     if select_ght.value=='ALL':
         filtre = etablissement.copy()
     else:
         filtre = etablissement.loc[etablissement.nom_ght==select_ght.value, :]
-    point_source.data = filtre
+    etab_interet_source.data = filtre
 
 def update_etablissement(attr, old, new):
     #reinitialize ght filter
     select_ght.value = '--'
     select_spe.value = '--'
-    #update map
-    point_source.data = etablissement\
+    #update red point : etablissement d'intérêt
+    etab_interet_source.data = etablissement\
     .loc[etablissement.etablissement.isin(mc.value), :]
-    #update table
-    tmp = flux.loc[flux.etablissement.isin(mc.value), :]
-    agg_df = tmp.groupby(['provenance'])['effectif_transfert'].sum().sort_values(ascending=False).reset_index()
-    agg_df['perc_transfert'] = round((agg_df['effectif_transfert'] / agg_df['effectif_transfert'].sum())*100,1)
-    
-    table_source.data = agg_df.copy()
-
-    #add the satellites on the graph
-    satellite_names = tmp['provenance'].drop_duplicates()
-    satellite_source.data = etablissement.loc[etablissement.etablissement.isin(satellite_names),:]
+    #display Etablissements de provenance or Etablissements de sortie on the map
+    if radio_group.active == 0:
+        #update table
+        tmp = flux.loc[flux.etablissement.isin(mc.value), :]
+        agg_df = tmp.groupby(['provenance'])['effectif_transfert'].sum().sort_values(ascending=False).reset_index()
+        agg_df['perc_transfert'] = round((agg_df['effectif_transfert'] / agg_df['effectif_transfert'].sum())*100,1)
+        table_source.data = agg_df.copy()
+        #add the satellites on the graph
+        satellite_names = tmp['provenance'].drop_duplicates()
+        etab_provenance_source.data = etablissement.loc[etablissement.etablissement.isin(satellite_names),:]
+        #reinit
+        etab_sortie_source.data = etablissement.loc[etablissement.etablissement=='--', ]
+        s = 'entrants'
+    else:
+        #update table
+        tmp = flux.loc[flux.provenance.isin(mc.value), :]
+        agg_df = tmp.groupby(['etablissement'])['effectif_transfert'].sum().sort_values(ascending=False).reset_index()
+        agg_df['perc_transfert'] = round((agg_df['effectif_transfert'] / agg_df['effectif_transfert'].sum())*100,1)
+        agg_df = agg_df.rename(columns = {'etablissement':'provenance'})
+        table_source.data = agg_df.copy()
+        #add the satellites on the graph
+        satellite_names = tmp['etablissement'].drop_duplicates()
+        etab_sortie_source.data = etablissement.loc[etablissement.etablissement.isin(satellite_names),:]
+        #reinit
+        etab_provenance_source.data = etablissement.loc[etablissement.etablissement=='--', ]
+        s = 'sortants'
+    p2.title = 'Spécialité - flux ' + s
 
     #update vbar
-    agg_df = tmp.groupby(['specialite'])['effectif_transfert']\
-    .sum().reset_index().sort_values(by='effectif_transfert', ascending=False)
+    if radio_group.active == 0:
+        agg_df = tmp.groupby(['specialite'], dropna=False).agg(
+            effectif_transfert = ('effectif_transfert', 'sum'),
+            details = ('provenance', lambda s: '/ '.join(s))
+        ).reset_index().sort_values(by='effectif_transfert', ascending=False)
+    else:
+        agg_df = tmp.groupby(['specialite'], dropna=False).agg(
+            effectif_transfert = ('effectif_transfert', 'sum'),
+            details = ('etablissement', lambda s: '/ '.join(s))
+        ).reset_index().sort_values(by='effectif_transfert', ascending=False)        
+    agg_df = agg_df.fillna('?')
     vbar_source.data = agg_df.copy()
     p2.x_range.factors = agg_df['specialite'].tolist()
+
+    
 
 
 def update_spe(attr, old, new):
@@ -203,14 +250,15 @@ def update_spe(attr, old, new):
     #filter the map by specialite
     names = flux.loc[flux.specialite==select_spe.value,'etablissement'].drop_duplicates()
     filtre = pd.merge(etablissement, names, how='inner')
-    point_source.data = filtre
+    etab_interet_source.data = filtre
 
 select_ght.on_change('value', update_ght)
 select_spe.on_change('value', update_spe)
 mc.on_change('value', update_etablissement)
+radio_group.on_change('active', update_etablissement)
 
 #define layout
-widgets = column(row(select_ght, mc), row(select_spe, paragraph), p2, table)
+widgets = column(row(select_ght, mc), row(select_spe, paragraph), radio_group, p2, table)
 layout = row(p, widgets)
 curdoc().add_root(layout)
 curdoc().title = 'Cartographie SSR'
